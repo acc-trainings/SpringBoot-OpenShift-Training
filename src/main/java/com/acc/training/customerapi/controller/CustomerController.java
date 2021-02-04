73661de13901a00deef12c8b1c93fbd7d4149ab8
rahul0309 <rahul.a0309@gmail.com>
2021-02-03 21:52:14 -0500

    modified yaml and related code change

--- src/main/java/com/acc/training/customerapi/controller/CustomerController.java
+++ src/main/java/com/acc/training/customerapi/controller/CustomerController.java
@@ -2,7 +2,6 @@ package com.acc.training.customerapi.controller;
 
 import javax.validation.Valid;
 
-import com.acc.training.customerapi.api.CreateCustomerApi;
 import com.acc.training.customerapi.api.CustomerApi;
 import com.acc.training.customerapi.model.Customer;
 import com.acc.training.customerapi.service.CustomerService;
@@ -14,7 +13,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CustomerController implements CustomerApi, CreateCustomerApi {
+public class CustomerController implements CustomerApi {
 
     @Autowired
     private CustomerService customerService;
@@ -28,7 +27,7 @@ public class CustomerController implements CustomerApi, CreateCustomerApi {
 
         Customer customer = customerService.getCustomer(id);
 
-        if(null == customer) {
+        if (null == customer) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
 
@@ -38,10 +37,10 @@ public class CustomerController implements CustomerApi, CreateCustomerApi {
     @Override
     public ResponseEntity<Customer> createCustomer(@Valid Customer body) {
 
-        if(null == body){
+        if (null == body) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-        
+
         Customer customer = customerService.createCustomer(body);
         return ResponseEntity.status(HttpStatus.OK).body(customer);
 
